b6d0f48f122f6aaf6e2c2764959ebf9366337510
Patrick Rault <prault@lbrmn-lnxub99.ric.broadcom.com>
2018-04-11 19:57:48 -0700

    SOC-7342: slight api change
    Bug: SOC-7342
    Change-Id: I4ef875e97e566cc14361e08993913cfedeadbeef
    Reviewed-on: http://gerrit-ccxsw.broadcom.net/62628
    Reviewed-by: Patrick Rault <patrick.rault@broadcom.com>
    Tested-by: Patrick Rault <patrick.rault@broadcom.com>

--- vkil_api.h
+++ vkil_api.h
@@ -1,5 +1,5 @@
-#ifndef vkapi_api_h__
-#define vkapi_api_h__
+#ifndef vkil_api_h__
+#define vkil_api_h__
  
 extern void vk_foo(void);
 
@@ -12,23 +12,48 @@ enum vkil_role_type
 
 enum vkil_command_type 
 {
-   VK_NONE = 0,
-   VK_IDLE = 1,
-   VK_RUN = 2,
-   VK_FLUSH  = 3,
+   VK_CMD_NONE = 0,
+   VK_CMD_IDLE = 1,
+   VK_CMD_RUN = 2,
+   VK_CMD_FLUSH  = 3,
+   VK_CMD_MAX = 0xFF
 };
+
+enum vkil_status_type 
+{
+   VK_STATE_UNLOADED = 0, // no hw is loaded
+   VK_STATE_READY = 1,
+   VK_STATE_IDLE = 1,
+   VK_STATE_RUN = 2,
+   VK_STATE_FLUSH  = 3,
+   VK_STATE_ERROR  = 0xFF,
+};
+
+
+// this structure is copied thru PCIE bridge and is currenlty limited to 16 bytes
+typedef struct _vkil_context_essential
+{
+   uint32_t   handle;     // host opaque handle
+   int8_t     card_id;    // 255 should be plenty enough
+                          // oxff mean the card_id is automatically determined by the driver
+   int8_t     queue_id;   // low, high priority 
+   int16_t    session_id; // allow the HW, to pool all the context pertaining to a single session
+                          // in case of the component die, it is expected the hw kills all the context pertaining to the session
+                         // all the component belonging to the session
+   enum       vkil_role_type component_role;
+}vkil_context_essential;
+
+
 
 typedef struct _vkil_context
 {
-   enum vkil_role_type component_role;
-   int32_t    session_id;
-   int32_t    card_id;
+   vkil_context_essential context_essential;
    void       *priv_data; // component role format dependent, 
 }vkil_context;
 
 
 typedef struct _VkIl {
-   int32_t (*init)(void ** handle, const VkContext * context);  
+   int32_t (*init)(void ** handle, const vkil_context * context);  
    int32_t (*deinit)(void *handle);
    int32_t  (*set_parameter)(const void *handle, const int32_t field, const void *value);  // static parameters
    int32_t  (*get_parameter)(const void *handle, const int32_t field, void **value); // set only in idling mode
@@ -57,4 +82,4 @@ typedef struct _VkIl {
 }VkIl;
 
  
-#endif  // foo_h__
+#endif  // vkil_api_h__
