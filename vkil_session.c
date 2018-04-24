065cea60e22fc5d259a6f0f47bb774ed023670ed
Victor Peng <victor.peng@broadcom.com>
2018-04-24 16:06:50 -0700

    SOC-7342: vkil_session: add stub for vkil_update_session_table
    - add a simple loop checking if pid is still running
    - decrement count and set all values to 0 (recycles the slot)
    - checks for empty entries when entry-finding since table rows can now be non-contiguous
    
    Bug: SOC-7342
    Change-Id: I3d48815a4a183d9c1254efe0e75ec37cdeadbeef
    Reviewed-on: http://gerrit-ccxsw.broadcom.net/62653
    Reviewed-by: Patrick Rault <patrick.rault@broadcom.com>
    Tested-by: Patrick Rault <patrick.rault@broadcom.com>

--- vkil_session.c
+++ vkil_session.c
@@ -2,12 +2,13 @@
 #include <unistd.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <signal.h>
 
 #include "vkil_session.h"
 
 #define VKIL_MAX_SESSION 10 // some fixe size for now
 #define VKIL_SHM_KEY     123 // some key value for now
-#define VKIL_SHM_SIZE    (1 + 3 * VKIL_MAX_SESSION) * sizeof(int) // some size for now
+#define VKIL_SHM_SIZE    sizeof(vkil_session_table) // some size for now
 #define VKIL_SHM_FLAG    0666
 #define VKIL_SHM_ID      shmget(VKIL_SHM_KEY, VKIL_SHM_SIZE, IPC_CREAT|VKIL_SHM_FLAG)
 
@@ -44,6 +45,15 @@ vkil_session_table* vkil_update_session_table(vkil_session_table *session_table)
 	// remove entries with exited pids
 	// update count
 	// will need to re-design the table
+	for(int i = 0; i < VKIL_MAX_SESSION; i++) {
+		if (kill(session_table->table[i].pid, 0)) {
+			// signal 0 is not sent, thus pid is not running
+			session_table->count--;
+			session_table->table[i].pid         = 0;
+			session_table->table[i].session_id  = 0;
+			session_table->table[i].card_id     = 0;
+		}
+	}
 	return session_table;
 }
 
@@ -62,14 +72,17 @@ int vkil_get_session_entry_index(vkil_session_table* session_table)
 	// entry in table
     int pid = getpid();
     int i;
-	for (i = 0; i < session_table->count; i++) {
+    int empty = VKIL_MAX_SESSION;
+	for (i = 0; i < VKIL_MAX_SESSION; i++) {
 		if(pid == session_table->table[i].pid)
 			return i;
+		else if(!session_table->table[i].pid && empty > i)
+			empty = i;
 	}
 
 	// entry not in table
-	if(i < VKIL_MAX_SESSION)
-		return session_table->count;
+	if(empty < VKIL_MAX_SESSION)
+		return empty;
 
 	return -1;
 }
@@ -92,7 +105,7 @@ uint8_t vkil_get_card_id()
 	int entry_index = vkil_get_session_entry_index(session_table);
 	if (entry_index < 0)
 		return -1;
-	if (session_table->count == 0 || entry_index == session_table->count)
+	if (session_table->count == 0 || !session_table->table[entry_index].pid)
 		vkil_create_session(session_table, entry_index);
 
 	return (uint8_t) session_table->table[entry_index].card_id;
