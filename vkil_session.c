20673d624c1622b03bf4fc6e586a26ab558e4134
Victor Peng <victor.peng@broadcom.com>
2018-04-25 14:44:06 -0700

    SOC-7342: vkil_session: inlcude sys/ipc.h and convert tabs to spaces
    As suggested:
        "The inclusion of <sys/types.h> and <sys/ipc.h> isn't required on Linux or by any version of POSIX. However, some old implementations required the inclusion of these header files, and the SVID also documented their inclusion. Applications intended to be portable to such old systems may need to include these header files."
    
    Bug: SOC-7342
    Change-Id: If6de4f11d2ce3e3efae39c7ad68e2b49deadbeef
    Reviewed-on: http://gerrit-ccxsw.broadcom.net/62656
    Reviewed-by: Patrick Rault <patrick.rault@broadcom.com>
    Tested-by: Patrick Rault <patrick.rault@broadcom.com>

--- vkil_session.c
+++ vkil_session.c
@@ -1,5 +1,6 @@
 #include <signal.h>
 #include <stdio.h>
+#include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -9,103 +10,129 @@
 #define VKIL_MAX_SESSION 10 // some fixe size for now
 #define VKIL_SHM_KEY     123 // some key value for now
 #define VKIL_SHM_SIZE    sizeof(vkil_session_table)
-#define VKIL_SHM_FLAG    0666
-#define VKIL_SHM_ID      shmget(VKIL_SHM_KEY, VKIL_SHM_SIZE, IPC_CREAT|VKIL_SHM_FLAG)
+#define VKIL_SHM_FLAG    IPC_CREAT | 0666
+#define VKIL_SHM_ID      shmget(VKIL_SHM_KEY, VKIL_SHM_SIZE, VKIL_SHM_FLAG)
+
+/*
+- modified from neverMind's answer on https://stackoverflow.com/questions/2143404/delete-all-system-v-shared-memory-and-semaphores-on-unix-like-systems
+- run the following script to erase the shm in case of testing
+- 0x0000007b is the key value in hex
+
+    #!/bin/bash
+    shmid=`ipcs -m | grep 0x0000007b | cut -f2 -d" "`
+    ipcrm -m $shmid;
+*/
+
+/*
+To do or/and re-evaluate:
+    - concurrency control since multiple processes will be accessing/modifying the shared table
+        - locks (?)
+    - underlying structure for the session table
+        - lookups could be optimized (?)
+        - table size
+    - finished sessions
+        - when to remove entry from table (right now 'pid active' check every time vkil_get_session_table() is called)
+    - shm key
+    - card selection
+        - refer to Valkyrie doc
+        - probably require arguments
+        - should it be in a seperate file (if complex)
+*/
 
 typedef struct _vkil_session_entry
 {
-	pid_t     pid;
-	uint8_t   session_id;
-	uint16_t  card_id;
+    pid_t     pid;
+    uint8_t   session_id;
+    uint16_t  card_id;
 } vkil_session_entry;
 
 typedef struct _vkil_session_table
 {
-	int count;
-	vkil_session_entry table[VKIL_MAX_SESSION];
+    int count;
+    vkil_session_entry table[VKIL_MAX_SESSION];
 } vkil_session_table;
 
 uint8_t vkil_select_card()
 {
-	// some mechanism to select the appropriate card
-	return 1;
+    // some mechanism to select the appropriate card
+    return 1;
 }
 
 void vkil_create_session(vkil_session_table* session_table, int entry_index)
 {
-	session_table->count++;
-	session_table->table[entry_index].pid = getpid();
-	session_table->table[entry_index].session_id = entry_index + 1; // use the index+1 as a session id for now
-	session_table->table[entry_index].card_id = vkil_select_card();
+    session_table->count++;
+    session_table->table[entry_index].pid = getpid();
+    session_table->table[entry_index].session_id = entry_index + 1; // use the index+1 as a session id for now
+    session_table->table[entry_index].card_id = vkil_select_card();
 }
 
 vkil_session_table* vkil_update_session_table(vkil_session_table *session_table)
 {
-	// remove entries with exited pids
-	// update count
-	// will need to re-design the table
-	for(int i = 0; i < VKIL_MAX_SESSION; i++) {
-		if (kill(session_table->table[i].pid, 0)) {
-			// signal 0 is not sent, thus pid is not running
-			session_table->count--;
-			session_table->table[i].pid         = 0;
-			session_table->table[i].session_id  = 0;
-			session_table->table[i].card_id     = 0;
-		}
-	}
-	return session_table;
+    // remove entries with exited pids
+    // update count
+    // will need to re-design the table
+    for(int i = 0; i < VKIL_MAX_SESSION; i++) {
+        if (kill(session_table->table[i].pid, 0)) {
+            // signal 0 is not sent, thus pid is not running
+            session_table->count--;
+            session_table->table[i].pid         = 0;
+            session_table->table[i].session_id  = 0;
+            session_table->table[i].card_id     = 0;
+        }
+    }
+    return session_table;
 }
 
 vkil_session_table* vkil_get_session_table()
 {
-	return vkil_update_session_table((vkil_session_table *) shmat(VKIL_SHM_ID, NULL, 0));
+    return vkil_update_session_table((vkil_session_table *) shmat(VKIL_SHM_ID, NULL, 0));
 }
 
 int vkil_get_session_entry_index(vkil_session_table* session_table)
 {
-	// table empty
-	if(session_table->count == 0)
-		return 0;
+    // table empty
+    if(session_table->count == 0)
+        return 0;
 
-	// entry in table
+    // entry in table
     int pid = getpid();
     int i;
     int empty = VKIL_MAX_SESSION;
-	for (i = 0; i < VKIL_MAX_SESSION; i++) {
-		if(pid == session_table->table[i].pid)
-			return i;
-		else if(!session_table->table[i].pid && empty > i)
-			empty = i;
-	}
+    for (i = 0; i < VKIL_MAX_SESSION; i++) {
+        if(pid == session_table->table[i].pid)
+            return i;
+        else if(!session_table->table[i].pid && empty > i)
+            empty = i;
+    }
 
-	// entry not in table and got a slot
-	if(empty < VKIL_MAX_SESSION)
-		return empty;
+    // entry not in table and got a slot
+    if(empty < VKIL_MAX_SESSION)
+        return empty;
 
-	// entry not in table and table full
-	return -1;
+    // entry not in table and table full
+    return -1;
 }
 
 uint16_t vkil_get_session_id()
 {
-	vkil_session_table* session_table = vkil_get_session_table();
-	int entry_index = vkil_get_session_entry_index(session_table);
-	if (entry_index < 0)
-		return 0;
-	if (session_table->count == 0 || !session_table->table[entry_index].pid)
-		vkil_create_session(session_table, entry_index);
+    vkil_session_table* session_table = vkil_get_session_table();
+    int entry_index = vkil_get_session_entry_index(session_table);
+    if (entry_index < 0)
+        return 0;
+    if (session_table->count == 0 || !session_table->table[entry_index].pid)
+        vkil_create_session(session_table, entry_index);
 
-	return session_table->table[entry_index].session_id;
+    return session_table->table[entry_index].session_id;
 }
 
 uint8_t vkil_get_card_id()
 {
-	vkil_session_table* session_table = vkil_get_session_table();
-	int entry_index = vkil_get_session_entry_index(session_table);
-	if (entry_index < 0)
-		return 0;
-	if (session_table->count == 0 || !session_table->table[entry_index].pid)
-		vkil_create_session(session_table, entry_index);
+    vkil_session_table* session_table = vkil_get_session_table();
+    int entry_index = vkil_get_session_entry_index(session_table);
+    if (entry_index < 0)
+        return 0;
+    if (session_table->count == 0 || !session_table->table[entry_index].pid)
+        vkil_create_session(session_table, entry_index);
 
-	return session_table->table[entry_index].card_id;
+    return session_table->table[entry_index].card_id;
 }
