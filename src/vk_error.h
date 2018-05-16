2bdc07f4ed5a855a8192ed7dd1120e0af0750792
Patrick Rault <prault@lbrmn-lnxub99.ric.broadcom.com>
2018-05-16 16:07:43 -0700

    SOC-7372 add the VKSIM layer index and prevent error type double negation
    add the VKSIM layer to be used by vksim_error.h
    add safeguard in case of parameters are passed already negative
    
    Change-Id: I39c272008790e37d1474d2c0b5dc7e63cacf8405
    Reviewed-on: http://gerrit-ccxsw.broadcom.net/62944
    Reviewed-by: Scott Branden <scott.branden@broadcom.com>
    Reviewed-by: Patrick Rault <patrick.rault@broadcom.com>
    Tested-by: Patrick Rault <patrick.rault@broadcom.com>

--- src/vk_error.h
+++ src/vk_error.h
@@ -1,19 +1,19 @@
-#ifndef vk_error_h__
-#define vk_error_h__
+#ifndef VK_ERROR_H__
+#define VK_ERROR_H__
 
-#include <stdint.h>
 #include <errno.h>
+#include <stdint.h>
 
 #define VKERROR_MAKE(layer,role,function,type)  \
-(-(((layer&0x7F)<<24) | ((role&0xFF)<<16) | \
-((function&0xFF)<<8)|  (((type&0xFF)<<0))))
-
+(-(((abs(layer)&0x7F) << 24) | ((abs(role)&0xFF) << 16) | \
+((abs(function)&0xFF) <<  8) | (((abs(type)&0xFF) << 0))))
 
 typedef enum {
     VKAPI       = 0,
-    VKIL	    = 1,
-    VKDRV	    = 3,
+    VKIL        = 1,
+    VKDRV       = 3,
+    VKSIM       = 4,
     VKLAYER_MAX = 0x7F
 }  vk_layer_t;
 
-#endif  // vkil_error_h__
+#endif
