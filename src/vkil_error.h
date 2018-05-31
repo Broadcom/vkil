52a7204d26d7da1089d24c23516eb49cb5f8e366
Victor Peng <victor.peng@broadcom.com>
2018-05-31 09:57:20 -0700

    SOC-7372: Clean, document and update vkil_error function
    * Re-structure to respect linux kernel standard
    * Add doxygen comment
    * Update vkil_error() to use vk_error.h vk_util.h macros
    
    Change-Id: I39ddfcf01e37019c60c1dc1ab6d6126fab50451d
    Reviewed-on: http://gerrit-ccxsw.broadcom.net/63857
    Reviewed-by: vpx_checkpatch status <vpx_checkpatch@broadcom.com>
    Reviewed-by: Ccxsw Build <ccxsw.build@broadcom.com>
    Tested-by: Ccxsw Build <ccxsw.build@broadcom.com>
    Reviewed-by: Patrick Rault <patrick.rault@broadcom.com>

--- src/vkil_error.h
+++ src/vkil_error.h
@@ -1,40 +1,56 @@
-#ifndef vkil_error_h__
-#define vkil_error_h__
+/*
+ * Copyright 2018 Broadcom
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation (the "GPL").
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License version 2 (GPLv2) for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 (GPLv2) along with this source code.
+ */
 
-
+#ifndef VKIL_ERROR_H
+#define VKIL_ERROR_H
 
 #include <string.h>
 #include "vk_error.h"
+#include "vk_utils.h"
 
-#ifndef VK_EOL
-#define VK_EOL "endoflist"
-#endif
+#define VKILERROR(type) VKERROR_MAKE(VKIL, 0, vkil_error(__func__), type)
 
-static int32_t vkil_error(const char * functionname)
+/**
+ * Generate an index number from a function name
+ * To be used to build an error code
+ *
+ * @param functionname function name
+ * @return             function index
+ */
+static int32_t vkil_error(const char *functionname)
 {
-    static char * vkil_fun_list[] = {
-                        "undefined",
-                        "vkil_init",
-                        "vkil_deinit",
-                        "vkil_set_parameter",
-                        "vkil_get_parameter",
-                        "vkil_send_buffer",
-                        "vkil_receive_buffer",
-                        "vkil_upload_buffer",
-                        "vkil_download_buffer",
-                        "vkil_uploaded_buffer",
-                        "vkil_downloaded_buffer",
-                        VK_EOL};
-    int32_t i=0;
-    do{
-        i++;
-        if (!strcmp(vkil_fun_list[i],functionname))
-            return i;
-    } while (strcmp(vkil_fun_list[i],VK_EOL));
-    return 0;
+	static const char * const vkil_fun_list[] = {
+		"undefined",
+		"vkil_init",
+		"vkil_deinit",
+		"vkil_set_parameter",
+		"vkil_get_parameter",
+		"vkil_send_buffer",
+		"vkil_receive_buffer",
+		"vkil_upload_buffer",
+		"vkil_download_buffer",
+		"vkil_uploaded_buffer",
+		"vkil_downloaded_buffer"};
+	int32_t i;
+
+	for (i = 1; i < VK_ARRAY_SIZE(vkil_fun_list); i++) {
+		if (!strcmp(vkil_fun_list[i], functionname))
+			return i;
+	}
+	return 0;
 };
 
-
-#define VKILERROR(type) VKERROR_MAKE(VKIL,0,vkil_error(__FUNCTION__),type)
-
-#endif  // vkil_error_h__
+#endif
