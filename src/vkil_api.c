337f88ba624d3d7a9e183b1a093c53922703a853
Patrick Rault <prault@lbrmn-lnxub99.ric.broadcom.com>
2018-07-26 17:11:08 -0700

    SOC-7373: restructure and complete the creation of the context on init
    the command to create the context on the vk card is done in vkil_init1_ctx
    vkil_init1_ctx is waiting for the vk card to return the ctx handle then
    populate the context_essential structure with it.
    
    Change-Id: I5c803734cba97b8230c0565d0e85136b93c25736
    Reviewed-on: http://gerrit-ccxsw.broadcom.net/66933
    Reviewed-by: vpx_checkpatch status <vpx_checkpatch@broadcom.com>
    Reviewed-by: Ccxsw Build <ccxsw.build@broadcom.com>
    Tested-by: Ccxsw Build <ccxsw.build@broadcom.com>
    Reviewed-by: Patrick Rault <patrick.rault@broadcom.com>

--- src/vkil_api.c
+++ src/vkil_api.c
@@ -29,111 +29,93 @@
  * Currently, it is limited to 16 bytes
  */
 typedef struct _vkil_context_internal {
-	int fd_dummy;
+	int fd;
 } vkil_context_internal;
 
-/**
- * Initialize a vkil_context
- * The first pass creates the vkil_context, done in s/w
- * The second pass invokes the h/w
- *
- * @param handle    reference of a handle to a vkil_context
- * @return          zero on success, error codes otherwise
- */
-int32_t vkil_init(void **handle)
+
+static int32_t vkil_init1_ctx(void *handle)
 {
-	int ret;
+	int32_t ret, i;
+	vkil_context *ilctx = handle;
+	vkil_context_internal *ilpriv;
+	host2vk_msg msg2vk;
+	vk2host_msg msg2host;
 	char dev_name[30]; /* format: /dev/bcm-vk.x */
 
-	VKIL_LOG(VK_LOG_DEBUG, "");
+	VK_ASSERT(!ilctx->priv_data);
+	/*
+	 * we know the context, but this one has not been created yet on the
+	 * vk card,
+	 */
+	ilctx->context_essential.handle = VK_NEW_CTX;
 
-	if (*handle == NULL) {
-		ret = vk_mallocz(handle, sizeof(vkil_context));
-		if (ret)
-			goto fail_malloc;
-		/* no information on the component, just return the handle */
-	} else {
-		vkil_context *ilctx = *handle;
+	/* we first get the session and card id*/
+
+	ilctx->context_essential.session_id = vkil_get_session_id();
+	if (ilctx->context_essential.session_id < 0)
+		goto fail_session;
+
+	ilctx->context_essential.card_id = vkil_get_card_id();
+	if (ilctx->context_essential.card_id < 0)
+		goto fail_session;
+
+	if (!snprintf(dev_name, sizeof(dev_name), "/dev/bcm-vk.%d",
+			ilctx->context_essential.card_id))
+		goto fail_session;
+
+	/* the priv_data structure size could be component specific */
+	ret = vk_mallocz(&ilctx->priv_data, sizeof(vkil_context_internal));
+	if (ret)
+		goto fail_malloc;
+
+	ilpriv = ilctx->priv_data;
+	ilpriv->fd = vkdrv_open(dev_name, O_RDWR);
+
+	VKIL_LOG(VK_LOG_DEBUG, "ilpriv->fd: %i\n", ilpriv->fd);
+
+	msg2vk.queue_id = ilctx->context_essential.queue_id;
+	msg2vk.function_id = vkil_get_function_id("init");
+	msg2vk.size        = 0;
+	msg2vk.context_id  = ilctx->context_essential.handle;
+	if (msg2vk.context_id == VK_NEW_CTX) {
 		/*
-		 * this is defined by the vk card
-		 * (expected to be the address on the valkyrie card)
+		 * the context is not yet existing on the device the arguments
+		 * carries the context essential allowing the device to create
+		 * it
 		 */
-		ilctx->context_essential.handle = 0;
-
-		ilctx->context_essential.session_id = vkil_get_session_id();
-		if (ilctx->context_essential.session_id < 0)
-			goto fail_session;
-
-		ilctx->context_essential.card_id = vkil_get_card_id();
-		if (ilctx->context_essential.card_id < 0)
-			goto fail_session;
-
-		if (!snprintf(dev_name, sizeof(dev_name), "/dev/bcm-vk.%d",
-			     ilctx->context_essential.card_id))
-			goto fail_session;
-
-		VKIL_LOG(VK_LOG_DEBUG, "session_id: %i\n",
-			 ilctx->context_essential.session_id);
-		VKIL_LOG(VK_LOG_DEBUG, "card_id: %i dev name %s\n",
-			 ilctx->context_essential.card_id, dev_name);
-
-		if (!ilctx->priv_data) {
-			vkil_context_internal *ilpriv;
-			host2vk_msg message;
-
-			/*
-			 * we know the component, but this one has not been
-			 * created yet, the priv_data structure size could be
-			 * component specific
-			 */
-			ret = vk_mallocz(&ilctx->priv_data,
-					 sizeof(vkil_context_internal));
-			if (ret)
-				goto fail_malloc;
-
-			ilpriv = ilctx->priv_data;
-			ilpriv->fd_dummy = vkdrv_open(dev_name, O_RDWR);
-
-			VKIL_LOG(VK_LOG_DEBUG, "ilpriv->fd_dummy: %i\n",
-				 ilpriv->fd_dummy);
-
-			message.queue_id    = ilctx->context_essential.queue_id;
-			message.function_id = vkil_get_function_id("init");
-			message.size        = 0;
-			message.context_id  = ilctx->context_essential.handle;
-			if (message.context_id == VK_NEW_CTX) {
-				/*
-				 * the context is not yet existing on the device
-				 * the arguments carries the context essential
-				 * allowing the device to create it
-				 */
-				memcpy(message.args, &ilctx->context_essential,
-				       sizeof(vkil_context_essential));
-			}
-
-			VKIL_LOG(VK_LOG_DEBUG, "&message: %x\n",
-				 &message);
-			VKIL_LOG(VK_LOG_DEBUG, "message.function_id: %x\n",
-				 message.function_id);
-			VKIL_LOG(VK_LOG_DEBUG, "message.size: %d\n",
-				 message.size);
-			VKIL_LOG(VK_LOG_DEBUG, "message.context_id: %x\n",
-				 message.context_id);
-
-			vkdrv_write(ilpriv->fd_dummy, &message,
-				    sizeof(message));
-
-			/*
-			 * Here, we will need to wait (poll_wait()) for the HW
-			 * to create the component.
-			 * vkdrv_read() to get the status of the hw
-			 */
-
-			VKIL_LOG(VK_LOG_DEBUG, "card inited %x\n",
-				 ilpriv->fd_dummy);
-		}
+		memcpy(msg2vk.args, &ilctx->context_essential,
+			sizeof(vkil_context_essential));
 	}
+
+	vkdrv_write(ilpriv->fd, &msg2vk, sizeof(msg2vk));
+
+	memset(&msg2host, 0, sizeof(msg2host));
+
+	/*
+	 * in the init phase the card will instantiate some stuff we don't have
+	 * visibility at vkil, but it is expected this take longer time than
+	 * usual so we don't abort at the first timeout
+	 */
+	for (i = 0 ; i < 4; i++) {
+		ret = vkil_wait_probe_msg(vkdrv_read, ilpriv->fd, &msg2host,
+						sizeof(msg2host));
+		if (ret ==  (-ETIMEDOUT))
+			continue;
+		else
+			break;
+	}
+	if (ret < 0)
+		goto fail_message_queue;
+
+	ilctx->context_essential.handle = msg2host.context_id;
+
+	VKIL_LOG(VK_LOG_DEBUG, "card inited %d\n, with context_id=%llx",
+			ilpriv->fd, ilctx->context_essential.handle);
 	return 0;
+
+fail_message_queue:
+	VKIL_LOG(VK_LOG_ERROR, "failed message queue %d\n", ret);
+	return VKILERROR(ret);
 
 fail_malloc:
 	VKIL_LOG(VK_LOG_ERROR, "failed malloc\n");
@@ -141,7 +123,7 @@ fail_malloc:
 
 fail_session:
 	return VKILERROR(ENOSPC);
-};
+}
 
 /**
  * De-initializes a vkil_context, and its associated h/w contents
@@ -155,19 +137,60 @@ int32_t vkil_deinit(void **handle)
 
 	VKIL_LOG(VK_LOG_DEBUG, "");
 
-	VK_ASSERT(handle);
+	if (!ilctx) {
+		VKIL_LOG(VK_LOG_ERROR, "unexpected call\n");
+		return 0;
+	}
 
 	if (ilctx->priv_data) {
 		vkil_context_internal *ilpriv;
 
 		ilpriv = ilctx->priv_data;
-		if (ilpriv->fd_dummy)
-			vkdrv_close(ilpriv->fd_dummy);
+		if (ilpriv->fd)
+			vkdrv_close(ilpriv->fd);
 		vk_free((void **)&ilpriv);
 	}
 	vk_free(handle);
 
 	return 0;
+};
+
+/**
+ * Initialize a vkil_context
+ * The first pass creates the vkil_context, done in s/w
+ * The second pass invokes the h/w
+ *
+ * @param handle    reference of a handle to a vkil_context
+ * @return          zero on success, error codes otherwise
+ */
+int32_t vkil_init(void **handle)
+{
+	int32_t ret;
+
+	VKIL_LOG(VK_LOG_DEBUG, "");
+
+	if (*handle == NULL) {
+		ret = vk_mallocz(handle, sizeof(vkil_context));
+		if (ret)
+			goto fail_malloc;
+		/* no information on the component, just return the handle */
+	} else {
+		vkil_context *ilctx = *handle;
+
+		if (!ilctx->priv_data)
+			ret = vkil_init1_ctx(*handle);
+			if (ret)
+				goto fail;
+	}
+	return 0;
+
+fail_malloc:
+	VKIL_LOG(VK_LOG_ERROR, "failed malloc\n");
+	return VKILERROR(ret);
+
+fail:
+	vkil_deinit(handle);
+	return VKILERROR(ret);
 };
 
 /**
@@ -356,7 +379,7 @@ int32_t vkil_send_buffer(const void *component_handle,
 		message.args[0]     = buffer_handle; /* TODO: to clarify */
 		message.args[1]     = 0;
 
-		return vkdrv_write(ilpriv->fd_dummy, &message, sizeof(message));
+		return vkdrv_write(ilpriv->fd, &message, sizeof(message));
 	}
 	}
 	return 0;
--- src/vkil_utils.h
+++ src/vkil_utils.h
@@ -17,7 +17,30 @@
 #ifndef VKIL_UTILS_H
 #define VKIL_UTILS_H
 
+#include <unistd.h>
 #include "vk_utils.h"
+
+
+/**
+ * try to call a function with given parameters and timeout afetr VK_TIMEOUT_MS
+ * @param pointer to function
+ * @param handle to the component
+ * @param message to process
+ * @return zero if success otherwise error message
+ */
+ssize_t vkil_wait_probe_msg(ssize_t (*f)(int fd, void *buf, size_t nbytes),
+			int fd, void *buf, size_t nbytes)
+{
+	int32_t ret, i;
+
+	for (i = 0 ; i < VK_TIMEOUT_MS ; i++) {
+		ret =  f(fd, buf, nbytes);
+		if (ret >= 0)
+			return ret;
+		usleep(1);
+	}
+	return (-ETIMEDOUT); /* if we are here we have timed out */
+}
 
 #define VKIL_LOG(...) vk_log(__func__, __VA_ARGS__)
 
