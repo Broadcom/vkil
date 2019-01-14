c48652c00e717b5d174affa24b1b021f06ae21a4
Desmond Yan <desmond.yan@broadcom.com>
2019-01-14 10:46:54 -0800

    SOC-8146: Change vkil to use common macros
    In a previous commit, a common macro sharable between vkil and vksim
    has been created.  This change convert the vkil to use the new macro.
    
    Change-Id: Id2ff89e683003decbb2740811b8315282510ea40
    Reviewed-on: http://gerrit-ccxsw.broadcom.net/78382
    Reviewed-by: vpx_checkpatch status <vpx_checkpatch@broadcom.com>
    Reviewed-by: Ccxsw Build <ccxsw.build@broadcom.com>
    Tested-by: Ccxsw Build <ccxsw.build@broadcom.com>
    Reviewed-by: Desmond Yan <desmond.yan@broadcom.com>
    Reviewed-by: Scott Branden <scott.branden@broadcom.com>

--- src/vkil_utils.h
+++ src/vkil_utils.h
@@ -19,42 +19,11 @@
 
 #include "vk_utils.h"
 
-/*
- * In the long-run, all the log related core macros will be moved
- * to vk_utils.h, ie, the following defines will be removed eventually.
- * *** Add the check !defined() for now, which allows us to
- * override it in the vksim's vk_utils.h, so that check-in in
- * vksim would build and pass smoke, as long as it has it defined in
- * a new way.
- */
-#if !defined(VKIL_LOG)
-#define VKIL_LOG(...) vk_log(__func__, __VA_ARGS__)
-#endif
+#define VKIL_LOG_HOST2VK_MSG(loglevel, msg)				 \
+		VK_H2VK_LOG(VKIL_LOG, loglevel, msg)
 
-#define VKIL_LOG_HOST2VK_MSG(loglevel, msg) VKIL_LOG(loglevel,		\
-	"host2vk_msg=%p: function_id=%d, size=%d, queue_id=%d, "	\
-	"msg_id=0x%x, context_id=0x%x args[0]=0x%x, args[1]=0x%x",	\
-	msg,								\
-	((host2vk_msg *)msg)->function_id,				\
-	((host2vk_msg *)msg)->size,					\
-	((host2vk_msg *)msg)->queue_id,					\
-	((host2vk_msg *)msg)->msg_id,					\
-	((host2vk_msg *)msg)->context_id,				\
-	((host2vk_msg *)msg)->args[0],					\
-	((host2vk_msg *)msg)->args[1])
-
-#define VKIL_LOG_VK2HOST_MSG(loglevel, msg) VKIL_LOG(loglevel,		\
-	"vk2host_msg=%p: function_id=%d, size=%d, queue_id=%d, "	\
-	"msg_id=0x%x, context_id=0x%x hw_status=%d, arg=0%x",		\
-	msg,								\
-	((vk2host_msg *)msg)->function_id,				\
-	((vk2host_msg *)msg)->size,					\
-	((vk2host_msg *)msg)->queue_id,					\
-	((vk2host_msg *)msg)->msg_id,					\
-	((vk2host_msg *)msg)->context_id,				\
-	((vk2host_msg *)msg)->hw_status,				\
-	((vk2host_msg *)msg)->arg)
-
+#define VKIL_LOG_VK2HOST_MSG(loglevel, msg)				 \
+		VK_VK2H_LOG(VKIL_LOG, loglevel, msg)
 
 int vkil_malloc(void **ptr, size_t size);
 int vkil_mallocz(void **ptr, size_t size);
